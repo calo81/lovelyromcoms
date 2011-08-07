f6d4a450b35313d627115ac276d03ce3e167189e
Carlo Scarioni <carlo.scarioni@gmail.com>
2011-08-07 18:13:10 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovelyromcoms::Application.config.secret_token = 'a63dfd254ab6a4c0a25d5b252d69e7a940bd45f9df03fb3a9248e99e5ff078523b26aa73caf5e9c9b7f9f031335011d28d6bfb4fd39f1f394ec4952d823228e6'
