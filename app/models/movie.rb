23ebfb2829e22fb8581822eaa29698499738b7ba
cscarioni <cscarioni@cscarioni-ubuntu-desktop.(none)>
2012-01-07 21:21:55 +0000

    retrieve indicators by user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,14 @@ class Movie
   def self.find_movie_of_the_day
     all[Random.rand(all.count)]
   end
+
+  def indicator_for_user(indicator,user)
+    return "" unless indicators[indicator]
+    self.indicators[indicator]["reviewers"].each do |reviewer|
+      if reviewer["id"]==user.id
+        return reviewer["value"]
+      end
+    end
+    return ""
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -13,4 +13,23 @@ describe Movie do
     p movie
     movie['title'].should=='17 Again'
   end
+
+  it "finds indicator value for user" do
+    movie = Movie.find_by_rotten_id(770680214)
+    user = User.new
+    user.id = 123
+    movie["indicators"] = {"couple_chemistry"=>{"total"=>4, "reviewers"=>[{"id"=>123, "value"=>4}]}}
+    value = movie.indicator_for_user("couple_chemistry", user)
+    value.should == 4
+  end
+
+  it "can set indicators for user and updates indicators" do
+    movie = Movie.find_by_rotten_id(770680214)
+    user = User.new
+    user.id = 123
+    movie["indicators"] = {"couple_chemistry"=>{"total"=>4, "reviewers"=>[{"id"=>123, "value"=>4}]}}
+    value = movie.indicator_for_user("couple_chemistry", user)
+    value.should == 4
+  end
+
 end
