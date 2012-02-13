7cdba921f79ab25d971374eaa600b4c0aed8c8a0
Carlo Scarioni <cscarioni@Carlos-MacBook-Air.local>
2012-02-13 13:03:57 +0000

    fixed slider values

--- public/javascripts/behaviours.js
+++ public/javascripts/behaviours.js
@@ -3,18 +3,17 @@
 // Sliders in movie edit
 
 $(function() {
-       var elements=new Array("couple_chemistry","she_handsome","he_handsome","dream_place","tear_rate","happy_ending","fun_factor","real_life_likely","sex_scenes");
-        $.each(elements,function(i,val){
-           $("#slider_"+val).slider({
-			range: "min",
-			value: 0,
-			min: 0,
-			max: 10,
-			slide: function( event, ui ) {
-				$("#"+val).val(ui.value );
-			}
-		});
-		$( "#"+val ).val($( "#slider_"+val ).slider( "value" ) );
+    var elements = new Array("couple_chemistry", "she_handsome", "he_handsome", "dream_place", "tear_rate", "happy_ending", "fun_factor", "real_life_likely", "sex_scenes");
+    $.each(elements, function(i, val) {
+        $("#slider_" + val).slider({
+            range: "min",
+            value: $("#" + val).val(),
+            min: 0,
+            max: 10,
+            slide: function(event, ui) {
+                $("#" + val).val(ui.value);
+            }
         });
+    });
 
-	});
+});
