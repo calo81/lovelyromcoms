bc0ef9fb872b8176decae0240f5a25c0195c547e
Carlo <carlo.scarioni@gmail.com>
2012-06-10 16:29:10 +0100

    fixed carousel and added two more carousels

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,27 +4,28 @@ class HomeController < ApplicationController
   def index
     @movie_of_the_day = Movie.find_movie_of_the_day
     recommendations
-    movie_list
+    @movie_list = movie_list("title")
+    @movie_list_he_handsome = movie_list("indicators.he_handsome.total")
+    @movie_list_she_handsome = movie_list("indicators.she_handsome.total")
   end
 
   private
-  def movie_list
-    @movie_list = if (params[:sort_by].nil?)
-                    Movie.retrieve_sorted_by(:title)
-                  else
-                    Movie.retrieve_sorted_by(params[:sort_by].to_sym)
-                  end
+  def movie_list(sorted_by)
+    list = Movie.retrieve_sorted_by(sorted_by.to_sym)
+    return list.map do |movie|
+      [movie, movie.avg_score]
+    end
   end
 
   def recommendations
     if current_user
-      @recommended_movies = recommender.recommend(current_user.id.to_s, 5)
+      @recommended_movies = recommender.recommend(current_user.id.to_s, 8)
       @recommended_movies = find_movies_for_each_id_returned(@recommended_movies)
       if !@recommended_movies.nil? and !@recommended_movies.empty?
         return
       end
     end
-    @recommended_movies = Movie.top_by_critics_rating(5).map { |movie| [movie, 4] }
+    @recommended_movies = Movie.top_by_critics_rating(8).map { |movie| [movie, 4] }
   end
 
   private
